d99c78a3bfb68abf960cee628d9f764918106249
Muhammad Saad Shaikh <119289745+mohdsaadshaikh@users.noreply.github.com>
2024-10-15 14:13:15 +0500

    Update options.js

--- server/src/constants/options.js
+++ server/src/constants/options.js
@@ -3,7 +3,7 @@ export const corsOpts = {
   credentials: true,
 };
 
-const cookieOptions = {
+export const cookieOptions = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   httpOnly: true,
   secure: process.env.NODE_ENV === "production", 
