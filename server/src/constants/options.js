bdbdec76538fd0105f3113b69dbef2f18d00c7ac
Muhammad Saad Shaikh <119289745+mohdsaadshaikh@users.noreply.github.com>
2024-10-19 21:33:07 +0500

    Update options.js

--- server/src/constants/options.js
+++ server/src/constants/options.js
@@ -7,5 +7,4 @@ export const cookieOptions = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "none",
 };
