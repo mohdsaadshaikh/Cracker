01e1f1a155e3db2ba48616dd18972d65b2b478bc
Muhammad Saad Shaikh <119289745+mohdsaadshaikh@users.noreply.github.com>
2024-10-19 13:41:30 +0500

    Update options.js

--- server/src/constants/options.js
+++ server/src/constants/options.js
@@ -5,7 +5,7 @@ export const corsOpts = {
 
 export const cookieOptions = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-  httpOnly: true,
+  httpOnly: false,
   secure: true,
   sameSite: "lax",
 };
