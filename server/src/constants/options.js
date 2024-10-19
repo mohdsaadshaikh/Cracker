11938655621b6687d4e53de4151c093f2b41264c
Muhammad Saad Shaikh <119289745+mohdsaadshaikh@users.noreply.github.com>
2024-10-19 21:48:18 +0500

    Update options.js

--- server/src/constants/options.js
+++ server/src/constants/options.js
@@ -1,5 +1,5 @@
 export const corsOpts = {
-  origin: ["https://cracker.up.railway.app", "http://localhost:5173", "http://localhost:6060"],
+  origin: ["https://cracker.up.railway.app", "http://localhost:5173", "http://localhost:6060", "https://cracker-app.netlify.app"],
   credentials: true,
 };
 
